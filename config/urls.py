348ccd0b1460bf3b5d258c16b1e62c4baf50be61
Rafał <steczkiewicz1998@Gmail.com>
2026-09-21 12:38:38 +0200

    Add login page

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("products/", include("catalog.urls")),
+    path("login/", include("accounts.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
